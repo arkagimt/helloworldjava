268eb0066e499b8454bc331d20245e2d78b51b29
arkagimt <104814192+arkagimt@users.noreply.github.com>
2022-08-24 08:20:47 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World at 8:16 AM");
+    System.out.println("Hello World at 8:20 AM");
     }
 }
